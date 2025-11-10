2d7c45bf028991df56163a1c6610cb4e59137b21
Alex Last <montevidio@inbox.ru>
2025-11-10 21:26:14 +0300

    version 1.4 (for lab5-6)

--- cmd/RIP/main.go
+++ cmd/RIP/main.go
@@ -30,7 +30,7 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
